e0bd02d87992bdf6fa7dd2ae4704dd247ad7770b
DrWho-cyber <77809141+DrWho-cyber@users.noreply.github.com>
2021-07-01 14:47:14 +0400

    setTimeout in sign in js.

--- signIn.js
+++ signIn.js
@@ -4,20 +4,21 @@ function checkUserData() {
 
     usersArray.forEach(element => {
         if (element.data.username == user && element.data.password == pass) {
-            window.location.href = "index.html"
-            window.localStorage.setItem('tempId',  537);
-            
-            if (element.data.username == "admin" && element.data.password == 0987){
-            window.location.href = "administrator.html"
-            window.localStorage.setItem('tempId',  389);
-        }
-        } else if (element.data.username !== user && element.data.password !== pass){
+            if (element.data.username == "admin" && element.data.password == 0987) {
+                window.location.href = "administrator.html"
+                window.localStorage.setItem('tempId', 389);
+            } else {
+                window.location.href = "index.html"
+                window.localStorage.setItem('tempId', 537);
+            }
+        } else {
             document.querySelector("#userName").value = '';
             document.querySelector("#password").value = '';
-            
-        }else{alert("error")}
+        }
 
     });
- 
+    setTimeout(function () {
+        alert("wrong user name of password");
+    }, 500);
 }
 
